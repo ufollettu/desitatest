bfec062331e0d6da56efd010835f64b7dd531b3f
Pasquale Merolle <pasqualemerolle@MacBook-Pro-di-Pasquale.local>
2022-08-09 02:46:31 +0200

    feat: first proto

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Desita Pagamento Multa test
 
+## Install Dependencies
+
+1. `npm install`
+
+## Start dev server
+
+1. `npm run dev`
+
 ## Zip the pages
 
-0. `npm install`
 1. `npm run zip`
 2. the dist.zip folder contains pages and assets files
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 // vite.config.js
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
-  root: "pages",
+  root: "src",
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "pages/index.html"),
-      },
-    },
+    outDir: "../dist",
   },
 });
