2bf7ca82ede8a75ad7e1629c6cca349226aaff87
Pasquale Merolle <pasqualemerolle@MacBook-Pro-di-Pasquale.local>
2022-08-10 12:03:49 +0200

    edit ppln

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: process.env.NODE_ENV === "production" ? "" : "public",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "public/index.html"),
-      },
-    },
-  },
+  root: "public",
+  base: "/public/",
 });
